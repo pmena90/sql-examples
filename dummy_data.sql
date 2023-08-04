c2e1e5d039f0a33b7df5ed365d36075ea10c1275
Pavel Mena <65360330+pmena90@users.noreply.github.com>
2023-08-04 11:01:14 +0200

    Create dummy_data.sql

---
+++ dummy_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO sales_table (sale_date, product_category, sale_amount)
+VALUES
+    ('2023-07-01', 'Electronics', 1500.00),
+    ('2023-07-02', 'Clothing', 500.50),
+    ('2023-07-03', 'Electronics', 1200.25),
+    ('2023-07-03', 'Books', 300.75),
+    ('2023-07-04', 'Toys', 250.00),
+    ('2023-07-05', 'Clothing', 800.80);
